f8478ea85476d5e6d92d89d296aa396e32fc2f9d
adrianiannantuono <aiannantuono@me.com>
2020-04-01 13:50:54 -0400

    not working yet

--- PartA/Exercise1/Chopstick.java
+++ PartA/Exercise1/Chopstick.java
@@ -2,19 +2,30 @@ public class Chopstick {
 	private int ID;
 // hint: use a local variable to indicate whether the chopstick is free
 //                        (lying on the table), e.g.  private boolean free;
+	private boolean free;
 
 	Chopstick(int ID) {
 		  this.ID = ID;
-
+			free = true;
 	}
 
 	synchronized void take() {
-
+		try {
+			wait();
+		} catch (InterruptedException e) {
+			System.out.println("A");
+		}
+		free = false;
 	}
 
 
 	synchronized void release() {
-
+		try {
+			notify();
+		} catch (IllegalMonitorStateException e) {
+			System.out.println("B");
+		}
+		free = true;
 	}
 
 	public int getID() {
