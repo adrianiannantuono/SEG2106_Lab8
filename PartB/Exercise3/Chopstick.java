9d8b2481c0cfda1593a2ccdde389dd4ebfb7c83d
adrianiannantuono <aiannantuono@me.com>
2020-04-03 13:43:10 -0400

    might work

--- PartB/Exercise3/Chopstick.java
+++ PartB/Exercise3/Chopstick.java
@@ -1,6 +1,7 @@
 import java.lang.*;
 public class Chopstick {
 	private int ID;
+	private long X = 2000;
 // hint: use a local variable to indicate whether the chopstick is free
 //                        (lying on the table), e.g.  private boolean free;
 	private boolean free;
@@ -20,7 +21,7 @@ public class Chopstick {
 
 			}
 			else{
-				wait();
+				wait(X);
 			}
 
 		}
